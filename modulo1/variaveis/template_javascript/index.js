af256514074bbf3797c0a2b197d29d205d9e9d02
Gabriel Salarini <gabrielsalarini@hotmail.com>
2022-02-08 20:42:08 -0300

    exercicios-variaveis

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,70 @@
+//Exercícios de interpretação de código
+
+//1 - 10
+//    10,5
+
+//2 - 10, 10, 10
+
+//3 - let horasTrabalho = prompt("Quantas horas você trabalha por dia?")
+//    let salarioPorDia = prompt("Quanto você recebe por dia?")
+//    alert(`Voce recebe ${t/p} por hora`)
+
+//Exercícios de escrita de código
+
+//const nome 
+//let idade 
+
+//console.log (typeof nome)
+//console.log (typeof idade) ("Olá" nome,  "você tem" idade "anos")
+
+// 1 - Foi impresso esse erro "Uncaught SyntaxError: Missing initializer in const declaration" pois não foi atribuido nenhum valor as variaveis.
+
+//const nome = prompt("Qual o seu nome?")
+//let idade = prompt("Qual sua idade?")
+
+//console.log (typeof nome)
+//console.log (typeof idade)
+
+//  Notei que quando o usuario responde as caixas do prompt as respostas ficam armazenadas no console.log
+//  e no console apareceu que as duas são strings
+
+//console.log ('Olá' nome,  "você tem" idade "anos')
+
+
+/*2 -
+let futebol = prompt ("Voce joga futebol?")
+let volei = prompt("Voce joga volei?")
+let tennis = prompt("Voce joga tennis?")
+
+console.log ("Voce joga futebol? " ,futebol)
+console.log ("Voce joga volei ? " ,volei)
+console.log ("Voce joga tennis ? " ,tennis)*/
+
+// 3 -
+
+    /*let a = "laranja"
+    let b = "coca"
+    let c = "vazio"
+
+    c = a
+    a = b
+    b = c
+
+    console.log(a, b, c)
+    
+    console.log("O novo valor de a é", a) 
+    console.log("O novo valor de b é", b)*/
+
+
+//Desafios
+
+    const a = prompt ("numero1")
+
+    const b = prompt ("numero2")
+
+    const x = (Number(a) + Number(b))
+
+    const y = (Number(a) * Number(b))
+
+    console.log (x, y)
+
